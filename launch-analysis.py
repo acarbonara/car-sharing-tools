98883e8fcb2a5d62688f8a3f219fbd8b922c69c3
acarbonara <a.carbonara91@gmail.com>
2017-08-16 15:58:24 +0200

    better launcher

--- launch-analysis.py
+++ launch-analysis.py
@@ -8,18 +8,17 @@ N = 5
 Ns = [5, 10, 15, 20, 25, 30]
 alpha = 0.5
 beta = 0.5
-alphas = [0.25, 0.50, 1.0]
-betas = [0.25, 0.50, 1.0]
+alphas = [0.00, 0.25, 0.50, 0.75, 1.0]
 
 city = sys.argv[1]
 
-for i in range(len(t)):
-    for j in range(len(d)):
-        city_dir = "matched-cars-final-" + city + "/"
-        if not os.path.exists(city_dir):
-            os.makedirs(city_dir)
-        for n in Ns:
-            script = 'python car-analysis.py ' + str(t[i]) + ' ' + str(d[j]) + ' ' + str(alpha) + ' ' + str(beta) + ' ' + str(n) + ' ' + city_dir
+for k in range(len(Ns)):
+    for i in range(len(t)):
+        for j in range(len(d)):
+            city_dir = "matched-cars-final-" + city + "/"
+            if not os.path.exists(city_dir):
+                os.makedirs(city_dir)
+            script = 'python car-analysis.py ' + str(t[i]) + ' ' + str(d[j]) + ' ' + str(alpha) + ' ' + str(beta) + ' ' + str(Ns[k]) + ' ' + city_dir
             os.system(script)
 
 script = 'mv *.dat ' + city_dir
@@ -31,9 +30,9 @@ for i in range(len(t)):
         if not os.path.exists(city_dir):
             os.makedirs(city_dir)
         for a in range(len(alphas)):
-            for b in range(len(betas)):
-                script = 'python car-analysis.py ' + str(t[i]) + ' ' + str(d[j]) + ' ' + str(alphas[a]) + ' ' + str(betas[b]) + ' ' + str(N) + ' ' + city_dir
-                os.system(script)
+            beta = 1.0 - a
+            script = 'python car-analysis.py ' + str(t[i]) + ' ' + str(d[j]) + ' ' + str(alphas[a]) + ' ' + str(beta) + ' ' + str(N) + ' ' + city_dir
+            os.system(script)
 
 script = 'mv *.dat ' + city_dir
 os.system(script)
